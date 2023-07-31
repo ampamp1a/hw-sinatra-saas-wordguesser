8c95bf54388c2c73f7f7f270dfb29aefdabe95d3
Napawan Nakasuwan <97944321+ampamp1a@users.noreply.github.com>
2023-07-31 09:25:53 +0000

    finished part_1_wordguesser

--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -7,6 +7,42 @@ class WordGuesserGame
 
   def initialize(word)
     @word = word
+    @guesses = ""
+    @wrong_guesses = ""
+  end
+
+  attr_accessor :word,:guesses,:wrong_guesses
+  
+  def guess(letter)
+    raise ArgumentError, "Input cannot be empty." if letter.nil? || letter.empty?
+    raise ArgumentError, "Input must be a single letter." unless letter.match?(/[A-Za-z]/)
+
+    letter.downcase!
+    if @word.include?(letter) and not @guesses.include?(letter)
+      @guesses = @guesses + letter
+      return true
+
+    elsif not @word.include?(letter) and not @wrong_guesses.include?(letter)
+        @wrong_guesses = @wrong_guesses + letter
+        return true
+    end
+
+    return false
+  end
+
+  def word_with_guesses
+    display = @word.chars.map { |letter| @guesses.include?(letter) ? letter : '-' }
+    display.join('')
+  end
+
+  def check_win_or_lose
+    if @word.chars.all? { |char| @guesses.include?(char) }
+      :win
+    elsif @wrong_guesses.size >= 7
+      :lose
+    else
+      :play
+    end
   end
 
   # You can test it by installing irb via $ gem install irb
